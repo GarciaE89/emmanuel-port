c6a3a75ee6c8b73dd3a35723f9cc6e19e7512ea0
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-08 13:20:20 -0700

    added nav and about components

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2 className=" about-title page-title">
+        <span>About Me</span>
+      </h2>
+      <div className="about-info flex-row">
+      <p className="self-description">
+      My name is Emmanuel Garcia, a full stack web developer. Attending the University of Utah's Full Stack Developer boot camp program. 
+      Throughout the program I learned proficencies in HTML, CCS, JavaScript, RESTful API's, SQL, MongoDB, Express.JS, Node.JS, SPA's. I have 10 years in banking,
+      and excited to add web developement to my bailiwick. 
+      </p>       
+      </div>
+    </section>
+  );
+}
+
+export default About;
