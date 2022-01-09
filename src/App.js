914b6d7a7171ad4aa9e2f1997282b4a9869cd543
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-08 20:15:26 -0700

    updates to App.js adding about as home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+
 // import './App.css'
 
 function App() {
+  const [pageSelected, setPageSelected] = useState ('home');
+  function getPage() {
+    if(pageSelected === "about" || pageSelected === "home") {
+      return <About></About>;
+    }
+    else if (pageSelected === "portfolio") {
+      return <Portfolio></Portfolio>;
+    }
+    else if (pageSelected === "resume"){
+      return <Resume></Resume>;
+    }
+    else if (pageSelected === "contact") {
+      return <Contact></Contact>;
+    }
+    
+    
+  }
   return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
+    <div className="App">
+      <div className="app-content">
+        <Nav pageSel={setPageSelected} selPage={pageSelected}></Nav>
+
+        {(getPage)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Contact/index.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../util/helpers";
+function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  function handleFormChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+  return (
+    <section className="contact-page">
+      <h1 className="page-title">Contact Me</h1>
+      <form id="contact-form" onSubmit={handleFormSubmit}>
+        <div>
+          <label
+            style={{
+              display: "block",
+              paddingBottom: "15px",
+              paddingTop: "5px",
+            }}
+            htmlFor="name"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            defaultValue={name}
+            onBlur={handleFormChange}
+            name="name"
+            placeholder="Name"
+            style={{ border: "1px solid #8c54ff" }}
+          />
+        </div>
+        <div>
+          <label
+            style={{
+              display: "block",
+              paddingBottom: "15px",
+              paddingTop: "5px",
+            }}
+            htmlFor="email"
+          >
+            Email Address:
+          </label>
+          <input
+            type="email"
+            defaultValue={email}
+            onBlur={handleFormChange}
+            name="email"
+            placeholder="Email"
+            style={{ border: "1px solid #8c54ff" }}
+          />
+        </div>
+        <div>
+          <label
+            style={{
+              display: "block",
+              paddingBottom: "15px",
+              paddingTop: "5px",
+            }}
+            htmlFor="message"
+          >
+            Message:
+          </label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleFormChange}
+            placeholder="Message"
+            style={{
+              border: "1px solid #8c54ff",
+              boxShadow: "2px 2px 1px var(--primary)",
+            }}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
