3245b7b12b795879e7688dcaf4fd65c05ce71a5f
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-08 20:50:36 -0700

    added resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Resume() {
+    return (
+        <div>
+            <div>
+                <h1 className="resume-title">
+                    Click{""}
+                    <span>
+                        <a href={resume} download> Here</a>
+                    </span>{""}
+                    to download a Emmanuel's resume.
+
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Resume;
