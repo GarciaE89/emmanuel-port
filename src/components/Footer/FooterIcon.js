c3b666ae469835255cfbdd3cfdab36ef7561eb1a
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-09 14:28:17 -0700

    bug fix to footericon

--- src/components/Footer/FooterIcon.js
+++ src/components/Footer/FooterIcon.js
@@ -4,7 +4,7 @@ import React from "react";
 function FooterIcon(props) {
   return (
     <li className="footer-icon">
-      <a href={props.url} target="_blank">
+      <a href={props.url} target="_blank" rel="noreferrer">
         <img src={props.iconImage} alt="footer icon"></img>
       </a>
     </li>
